17ac6f9717bc4138fd250cdb7495a17c86d85c59
cuong081004 <2251120336@ut.edu.vn>
2025-04-19 11:37:55 +0700

    lưu note vào database

--- NOTEAPP/app/src/main/java/com/example/noteapp/activities/MainActivity.kt
+++ NOTEAPP/app/src/main/java/com/example/noteapp/activities/MainActivity.kt
@@ -1,21 +1,35 @@
 package com.example.noteapp.activities
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.example.noteapp.R
+import androidx.lifecycle.ViewModelProvider
+import com.example.noteapp.databinding.ActivityMainBinding
+import com.example.noteapp.db.NoteDataBase
+import com.example.noteapp.repository.NoteRepository
+import com.example.noteapp.viewModel.NoteActivityViewModel
+import com.example.noteapp.viewModel.NoteActivityViewModelFactory
 
 class MainActivity : AppCompatActivity() {
+
+
+    lateinit var noteActivityViewModel: NoteActivityViewModel
+    private lateinit var binding: ActivityMainBinding
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        supportActionBar?.hide()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        try {
+            setContentView(binding.root)
+            val noteRepository= NoteRepository(NoteDataBase(this))
+            val noteActivityViewModelFactory= NoteActivityViewModelFactory(noteRepository)
+            noteActivityViewModel= ViewModelProvider(this,
+                noteActivityViewModelFactory)[NoteActivityViewModel::class.java]
+        } catch (e: Exception) {
+            Log.d("TAG", "Error")
         }
     }
 }
--- NOTEAPP/app/src/main/java/com/example/noteapp/fragments/NoteFragment.kt
+++ NOTEAPP/app/src/main/java/com/example/noteapp/fragments/NoteFragment.kt
@@ -1,61 +1,77 @@
 package com.example.noteapp.fragments
 
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.view.WindowManager
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.Navigation
 import com.example.noteapp.R
+import com.example.noteapp.activities.MainActivity
+import com.example.noteapp.databinding.FragmentNoteBinding
+import com.example.noteapp.utils.hideKeybroad
+import com.example.noteapp.viewModel.NoteActivityViewModel
+import com.google.android.material.transition.MaterialElevationScale
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [NoteFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class NoteFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class NoteFragment : Fragment(R.layout.fragment_note) {
+
+
+    private lateinit var noteBinding: FragmentNoteBinding
+    private val noteActivityViewModel: NoteActivityViewModel by activityViewModels()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+        exitTransition=MaterialElevationScale(false).apply {
+            duration=350
         }
+        enterTransition=MaterialElevationScale(true).apply {
+            duration=350
+        }
+
+
+
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_note, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        noteBinding=FragmentNoteBinding.bind(view)
+        val activity=activity as MainActivity
+        val navController= Navigation.findNavController(view)
+        requireView().hideKeybroad()
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(10)
+            activity.window.statusBarColor=Color.WHITE
+            activity.window.clearFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+            activity.window.statusBarColor=Color.parseColor("#9E9D9D")
+        }
+
+
+        noteBinding.addNoteFab.setOnClickListener {
+            noteBinding.appBarLayout.visibility= View.INVISIBLE
+            navController.navigate(NoteFragmentDirections.actionNoteFragmentToSaveOrUpdateFragment())
+        }
+
+
+        noteBinding.innerFab.setOnClickListener {
+            noteBinding.appBarLayout.visibility= View.INVISIBLE
+            navController.navigate(NoteFragmentDirections.actionNoteFragmentToSaveOrUpdateFragment())
+        }
+
+
+
+
+
+
+
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment NoteFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            NoteFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
 
